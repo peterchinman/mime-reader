6e0cd5533516320f0e549c54b06f4bf99c9fd719
Peter Chinman <peter.chinman@gmail.com>
2026-05-24 16:11:02 -0400

    Update errors

--- src/error.rs
+++ src/error.rs
@@ -34,16 +34,33 @@ impl std::fmt::Display for DictionaryError {
 
 #[derive(Debug)]
 pub enum ParseMeterError {
-    InvalidChar(char),
-    InvalidParenNesting,
+    InvalidChar { c: char, col: usize },
+    InvalidParenNesting { col: usize },
 }
 
 impl std::fmt::Display for ParseMeterError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ParseMeterError::InvalidChar(c) => write!(f, "invalid stress character '{}'", c),
-            ParseMeterError::InvalidParenNesting => write!(f, "invalid parentheses nesting"),
+            ParseMeterError::InvalidChar { c, col } => {
+                write!(f, "invalid stress character '{c}' at column {col}")
+            }
+            ParseMeterError::InvalidParenNesting { col } => {
+                write!(f, "invalid parentheses nesting at column {col}")
+            }
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct ParseMeterSchemeError {
+    pub line: usize,
+    pub source: ParseMeterError,
+}
+
+impl std::fmt::Display for ParseMeterSchemeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "line {}: {}", self.line + 1,
+          self.source)  // +1 for human-readable
     }
 }
 
@@ -74,6 +91,19 @@ impl std::fmt::Display for MeterMatchError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             MeterMatchError::FailedMatch => write!(f, "failed to match meter"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ParseRhymeError {
+    InvalidChar(char),
+}
+
+impl std::fmt::Display for ParseRhymeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseRhymeError::InvalidChar(c) => write!(f, "invalid rhyme character '{}'", c),
         }
     }
 }
